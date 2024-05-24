e80c5f48b01d5b3d369e8352ee4e2a74a28e3b55
Osas997 <larasatyt83@gmail.com>
2024-05-24 23:27:40 +0700

    coba slur

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,3 @@ app.listen(8000);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
-
-const data = [
-  {
-    id: 1,
-    name: "BRoook",
-    age: 20
-  },
-  {
-    id: 2,
-    name: "YHOHOHO",
-    age: 20
-  }
-]
